5d7792fe79e0b1979d4ab32512e744a67a899412
Juliana Zagaja <julianazagaja@icloud.com>
2025-11-20 08:56:45 -0500

    commit

---
+++ lesson 5 - exception handling/exceptions.py
@@ -0,0 +1,22 @@
+#common errors
+#1 - division by zero
+# print(10/0)
+
+#2 - value error
+# print(int("abc"))
+
+#3 - key error
+# grades = {"Bob": 90}
+# print(grades["Bill"])
+
+#4 - index errror
+# numbers = [1, 2, 3]
+# print(numbers[])
+
+#5 - name error
+# print(total)
+# total = 10
+
+#6 - syntax error
+# for i in range(5):
+#     print(i)
